779a9a5480eef2561e7e1618eed2a6992159a456
Falk Werner <falk.werner@gmx.net>
2024-01-29 20:45:44 +0100

    allow to specify csr filename

--- print-csr.py
+++ print-csr.py
@@ -1,12 +1,18 @@
 #!/usr/bin/env python3
 
+import argparse
 from cryptography import x509
 from cryptography.x509.oid import NameOID
 
-with open('ca.csr', "rb") as f:
-    data = f.read()
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser("print-csr")
+    parser.add_argument("filename", type=str)
+    args = parser.parse_args()
 
-csr = x509.load_pem_x509_csr(data)
+    with open(args.filename, "rb") as f:
+        data = f.read()
 
-commonName = csr.subject.get_attributes_for_oid(NameOID.COMMON_NAME)[0]
-print(commonName.value)
+    csr = x509.load_pem_x509_csr(data)
+
+    commonName = csr.subject.get_attributes_for_oid(NameOID.COMMON_NAME)[0]
+    print(commonName.value)
