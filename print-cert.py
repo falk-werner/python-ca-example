dbd072fabbf04dc1a87effde8e764188cea75d7f
Falk Werner <falk.werner@gmx.net>
2024-01-29 20:30:36 +0100

    add script to print certificates

---
+++ print-cert.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+
+import argparse
+from cryptography import x509
+
+
+def print_cert(filename: str):
+    with open(filename, "rb") as f:
+        data = f.read()
+    cert = x509.load_pem_x509_certificate(data)
+    print(f"serial          : 0x{cert.serial_number:x}")
+    print(f"not valid before: {cert.not_valid_before}")
+    print(f"not valid after : {cert.not_valid_after}")
+    print("issuer:")
+    print_name(cert.issuer)
+    print("subject:")
+    print_name(cert.subject)
+    print("Extensions:")
+    for extension in cert.extensions:
+            print(extension)
+
+def print_name(name):
+    for item in name:
+        print(f"\t{item.rfc4514_attribute_name}: {item.value}")
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(prog="print-cert",
+        description="print certificate")
+    parser.add_argument("filename", type=str)
+    args = parser.parse_args()
+    print_cert(args.filename)
